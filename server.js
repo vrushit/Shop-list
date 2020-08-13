ef53d9f1a8cb77da7a54ca8a827ff258b2fb4ed6
vrushit <bharuchavrushit@ymail.com>
2020-08-13 22:13:11 +0530

    Server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//Body Parser  Middleware
+
+app.use(bodyParser.json());
