559490f7efcb560fb1266c214876bf05999cbf0d
vrushit <bharuchavrushit@ymail.com>
2020-08-13 22:24:34 +0530

    Database linked:

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://merlin:merlin123@mern.9nnmp.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};
--- server.js
+++ server.js
@@ -7,3 +7,16 @@ const app = express();
 //Body Parser  Middleware
 
 app.use(bodyParser.json());
+
+// Connect to mongoDB
+const db = require("./config/keys").mongoURI;
+
+//Connection to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server Started on ${port}`));
