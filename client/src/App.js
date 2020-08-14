7ecbc094090e81e4603275774301414e2a93bd46
vrushit <bharuchavrushit@ymail.com>
2020-08-14 16:34:41 +0530

    Including the store

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ShoppingList from "./components/ShoppingList";
 import AppNavbar from "./components/AppNavbar";
-function App() {
-  return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
-  );
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
