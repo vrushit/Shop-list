35fc1a70eaa726037d4759c463b0fa433a7f5702
vrushit <bharuchavrushit@ymail.com>
2020-08-14 16:16:41 +0530

    Loading Dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
+import ShoppingList from "./components/ShoppingList";
 import AppNavbar from "./components/AppNavbar";
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
