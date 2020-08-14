220c2151f2e2925865c72a2cc3e05d9c1d281eae
vrushit <bharuchavrushit@ymail.com>
2020-08-14 16:46:00 +0530

    itemActions

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,5 +1,7 @@
 import uuid from "react-uuid";
 
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+
 const initialState = {
   items: [
     { id: uuid(), name: "Eggs" },
@@ -8,3 +10,14 @@ const initialState = {
     { id: uuid(), name: "Steak" },
   ],
 };
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_ITEMS:
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+}
