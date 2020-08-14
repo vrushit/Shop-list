321cd9d15ccaea096c2a15e8a1798fd649f39306
vrushit <bharuchavrushit@ymail.com>
2020-08-14 16:38:53 +0530

    itemReducer

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,10 @@
+import uuid from "react-uuid";
+
+const initialState = {
+  items: [
+    { id: uuid(), name: "Eggs" },
+    { id: uuid(), name: "Sweets" },
+    { id: uuid(), name: "Milk" },
+    { id: uuid(), name: "Steak" },
+  ],
+};
