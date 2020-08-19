634e9761bdef53d024eb50cde2eb35ecda648d57
vrushit <bharuchavrushit@ymail.com>
2020-08-19 21:15:36 +0530

    Adding Items to mongodb

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -21,7 +21,12 @@ export const deleteItem = (id) => {
 };
 
 export const addItem = (item) => (dispatch) => {
-  axios.post("/api/items", item);
+  axios.post("/api/items", item).then((res) =>
+    dispatch({
+      type: ADD_ITEM,
+      payload: res.data,
+    })
+  );
 };
 
 export const setItemsLoading = () => {
