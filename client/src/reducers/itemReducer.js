7ff0181136dd827f206a3d143e1e49ecd2a0992d
vrushit <bharuchavrushit@ymail.com>
2020-08-19 20:30:16 +0530

    removing static data

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -3,12 +3,7 @@ import uuid from "react-uuid";
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
 
 const initialState = {
-  items: [
-    { id: uuid(), name: "Eggs" },
-    { id: uuid(), name: "Sweets" },
-    { id: uuid(), name: "Milk" },
-    { id: uuid(), name: "Steak" },
-  ],
+  items: [],
 };
 
 export default function (state = initialState, action) {
